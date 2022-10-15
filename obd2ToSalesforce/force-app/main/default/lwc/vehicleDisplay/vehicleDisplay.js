5dea73796d8124d7da381e73d977457028f9352c
fishnorton <nortonfd@gmail.com>
2022-10-14 19:25:38 -0700

    Made some updates to the soql query and javascript, still need to fix it though

--- obd2ToSalesforce/force-app/main/default/lwc/vehicleDisplay/vehicleDisplay.js
+++ obd2ToSalesforce/force-app/main/default/lwc/vehicleDisplay/vehicleDisplay.js
@@ -1,15 +1,17 @@
 import { LightningElement, track, wire } from 'lwc';
+import uId from '@salesforce/user/Id';
 import sfdc_fetchCars from '@salesforce/apex/FetchCars.getCarsFromUser';
 
 export default class VehicleDisplay extends LightningElement {
-    @track car;
-    
-    get carLink() {
-        return '/' + this.car.Id
+    @track cars = [];
+
+    connectedCallback() {
+        console.log(uId);
+        console.log(this.cars);
     }
 
-    @wire(sfdc_fetchCars)
-    wiredCars(cars) {
-        this.car =cars;
+    @wire(sfdc_fetchCars, {userId: '$uId'})
+    wiredCars(returnedCars) {
+        this.cars = returnedCars.data;
     }
 }
